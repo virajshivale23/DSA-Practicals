6fcdacb77c90525ecb4e59034e2d79fbc53cf3c8
virajshivale95 <195629108+virajshivale23@users.noreply.github.com>
2025-10-01 07:10:22 +0530

    Add files via upload

---
+++ LAB1 Exercise.R
@@ -0,0 +1,28 @@
+#LAB 1 EXERCISE QUESTIONS
+
+
+#TO DISPLAY THE FIRST 15 ROWS OF MTCARS DATASET
+head (mtcars, 15)
+
+
+# TO FIND THE MAXIMUM AND MINIMUM Value of Sepal.lenght from iris
+
+#MAXIMUM VALUE
+max(iris$Sepal.Length)
+
+#MINIMUM VALUE
+min(iris$Sepal.Length)
+
+#CALCULATE MEAN OF THE VARIABLE mpg in mtcars
+
+mean(mtcars$mpg)
+
+
+#DISPLAY THE STRUCTURE OF AIRQUALITY DATASET
+
+str(airquality)
+
+
+# CHECK WHETHER THE NUMBER 100 IS GREATER THAN 50
+
+100>50
